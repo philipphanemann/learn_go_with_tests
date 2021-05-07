f76f854006d8b7069e72295d117c82c60d1e0ad6
Philipp Hanemann <philipp.hanemann@posteo.de>
2021-05-07 13:06:52 +0200

    french greeting

--- hello_test.go
+++ hello_test.go
@@ -29,4 +29,10 @@ func TestHello(t *testing.T) {
 		assertCorrectMessage(t, got, want)
 	})
 
+	t.Run("in French", func(t *testing.T) {
+		got := Hello("Elodie", "French")
+		want := "Bonjour, Elodie"
+		assertCorrectMessage(t, got, want)
+	})
+
 }
