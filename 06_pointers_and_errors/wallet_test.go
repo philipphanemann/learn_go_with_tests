0303279dbf17e135c2d5e2dc03c20be01a09ca67
Philipp Hanemann <philipp.hanemann@posteo.de>
2021-05-15 14:17:10 +0200

    refactor tests

--- 06_pointers_and_errors/wallet_test.go
+++ 06_pointers_and_errors/wallet_test.go
@@ -5,29 +5,25 @@ import (
 )
 
 func TestWallet(t *testing.T) {
+
+	assertBalance := func(t *testing.T, wallet Wallet, want Bitcoin) {
+		got := wallet.Balance()
+		if got != want {
+			t.Errorf("got %s want %s", got, want)
+		}
+	}
+
 	t.Run("Deposit", func(t *testing.T) {
 
 		wallet := Wallet{}
 
 		wallet.Deposit(Bitcoin(10))
-
-		got := wallet.Balance()
-
-		want := Bitcoin(10)
-
-		if got != want {
-			t.Errorf("got %s want %s", got, want)
-		}
+		assertBalance(t, wallet, Bitcoin(10))
 	})
 
 	t.Run("Withdraw", func(t *testing.T) {
 		wallet := Wallet{balance: Bitcoin(20)}
 		wallet.Withdraw(Bitcoin(5))
-		got := wallet.Balance()
-		want := Bitcoin(15)
-
-		if got != want {
-			t.Errorf("got %s want %s", got, want)
-		}
+		assertBalance(t, wallet, Bitcoin(15))
 	})
 }
